7b1113de1e94d4a7b713ada9c19e3e6c66649506
dishonestea <vishay8@gmail.com>
2020-07-31 17:01:35 -0500

    fixing dependencies

--- controllers/register.js
+++ controllers/register.js
@@ -3,7 +3,9 @@ const handleRegister = (req, res, db, bcrypt) => {
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
-  const hash = bcrypt.hashSync(password);
+
+  const hash = bcrypt.hashSync(password, 8);
+
     db.transaction(trx => {
       trx.insert({
         hash: hash,
